664da507278d9ad80925a049fc61c3e077542dc7
kd8lvt <redunderpant@gmail.com>
2024-10-29 18:54:55 -0400

    Feat: Fix Omen of Caro Invictus failing to play boss music Feat: Building indev now attaches the first 8 digits of the HEAD commit id.

--- src/main/java/com/kd8lvt/exclusionzone/item/CaroInvictusSummoner.java
+++ src/main/java/com/kd8lvt/exclusionzone/item/CaroInvictusSummoner.java
@@ -27,7 +27,7 @@ public class CaroInvictusSummoner extends Artifact {
         if (!Objects.requireNonNull(context.getPlayer()).isSneaking()) return super.useOnBlock(context);
         if (((BeaconBlockEntity) Objects.requireNonNull(w.getBlockEntity(context.getBlockPos()))).getBeamSegments().isEmpty()) return super.useOnBlock(context);
 
-        w.playSoundAtBlockCenter(context.getBlockPos().up(), ModSounds.get("caro_invictus_music"), SoundCategory.MASTER,1.0f,1.0f,true);
+        w.playSoundAtBlockCenter(context.getBlockPos().up(), ModSounds.get("mob.caro_invictus.music"), SoundCategory.MASTER,1.0f,1.0f,true);
 
         if (w.isClient) return super.useOnBlock(context);
         ServerWorld world = (ServerWorld)w;
